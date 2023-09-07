6dfb74115060e0c27bd1e02ca5e8230559d548be
marneo23 <martin.al.rodriguez24@gmail.com>
2023-09-07 00:31:45 -0300

    Eliminé las captions, quedaban flotando arriba de la imagen.

--- src/components/Inicio/CarouselCard.jsx
+++ src/components/Inicio/CarouselCard.jsx
@@ -9,10 +9,6 @@ function CarouselCard() {
                     src="/assets/imagen1.jpg"
                     alt="First slide"
                 />
-                <Carousel.Caption>
-                    <h5>First slide label</h5>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
@@ -20,10 +16,6 @@ function CarouselCard() {
                     src="assets/imagen2.jpg"
                     alt="Second slide"
                 />
-                <Carousel.Caption>
-                    <h5>Second slide label</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
@@ -31,14 +23,9 @@ function CarouselCard() {
                     src="/assets/imagen3.jpg"
                     alt="Third slide"
                 />
-                <Carousel.Caption>
-                    <h5>Third slide label</h5>
-                    <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
-                </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
+        
     );
 }
 
