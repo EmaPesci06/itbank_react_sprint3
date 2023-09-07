afb9d496557dd3a064d4fa0b1c10403ed97b7ced
Martin Rodriguez <martin.rodriguez@mcplusa.com>
2023-09-07 12:47:35 -0300

    Agregué los scripts para Cuentas

--- src/components/Cuentas/Cuentas.jsx
+++ src/components/Cuentas/Cuentas.jsx
@@ -1,13 +1,20 @@
-import { FormCuentas } from "./FormCuentas"
-import { TableCuentas } from "./TableCuentas"
+import React, { useState } from 'react';
+import { FormCuentas } from './FormCuentas';
+import { TableCuentas } from './TableCuentas';
 
 export const Cuentas = () => {
+    const [accounts, setAccounts] = useState([]);
+
+    const addAccount = (newAccount) => {
+        setAccounts([...accounts, newAccount]);
+    };
+
     return (
         <main className="main">
             <div className="containerCuenta w-xl">
-                <TableCuentas></TableCuentas>
-                <FormCuentas></FormCuentas>
+                <TableCuentas accounts={accounts} />
+                <FormCuentas addAccount={addAccount} />
             </div>
         </main>
-    )
-}
+    );
+};
--- src/components/Cuentas/FormCuentas.jsx
+++ src/components/Cuentas/FormCuentas.jsx
@@ -1,33 +1,81 @@
+import React, { useState } from 'react';
 
-export const FormCuentas = () => {
+export const FormCuentas = ({ addAccount }) => {
+    const [inputNumberAccount, setInputNumberAccount] = useState('');
+    const [inputAccount, setInputAccount] = useState('');
+    const [balance, setBalance] = useState('');
+    const [selectedChoice, setSelectedChoice] = useState('Cuenta de Ahorro');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const regex = /^[A-Za-z\s]+$/;
+
+        if (
+            inputNumberAccount.length === 9 &&
+            regex.test(inputAccount) &&
+            !isNaN(balance) &&
+            balance >= 0
+        ) {
+            // crear cuenta
+            const newAccount = {
+                number: inputNumberAccount,
+                name: inputAccount,
+                balance: `$ ${balance}`,
+                type: selectedChoice,
+            };
+
+            // addAcount interactua con el hook del componente padre
+            addAccount(newAccount);
+
+            //limpiar forms una vez termino
+            setInputNumberAccount('');
+            setInputAccount('');
+            setBalance('');
+            setSelectedChoice('Cuenta de Ahorro');
+        } else {
+            console.log(inputNumberAccount.length === 9 &&
+                regex.test(inputAccount) &&
+                !isNaN(balance) &&
+                balance >= 0);
+            alert(
+                'Verifica los campos. El número de cuenta debe tener al menos 9 dígitos, el nombre debe contener solo letras y el saldo no puede estar vacío.'
+            );
+        }
+    };
+
+    const handleChoiceChange = (e) => {
+        setSelectedChoice(e.target.value);
+    };
+
     return (
         <>
-            <form id="formAccount">
+            <form id="formAccount" onSubmit={handleSubmit}>
                 <h3 className="text-center mt-3">Agregar Cuenta</h3>
                 <div className="mb-3">
-                    <label for="numberAccount" className="form-label">Numero de Cuenta</label>
-                    <input type="number" className="form-control" id="numberAccount" aria-describedby="emailHelp" />
+                    <label htmlFor="numberAccount" className="form-label">Numero de Cuenta</label>
+                    <input type="number" className="form-control" id="numberAccount" aria-describedby="emailHelp" value={inputNumberAccount} onChange={(e) => setInputNumberAccount(e.target.value)} />
                     <div id="emailHelp" className="form-text">Deben ser de 9 numeros</div>
                 </div>
                 <div className="mb-3">
-                    <label for="nameAccount" className="form-label">Nombre de la Cuenta</label>
-                    <input type="text" className="form-control" id="nameAccount" />
+                    <label htmlFor="nameAccount" className="form-label">Nombre de la Cuenta</label>
+                    <input type="text" className="form-control" id="nameAccount" value={inputAccount} onChange={(e) => setInputAccount(e.target.value)} />
                 </div>
 
                 <div className="mb-3">
-                    <label for="balance" className="form-label">Saldo de la Cuenta</label>
-                    <input type="number" className="form-control" id="balance" />
+                    <label htmlFor="balance" className="form-label">Saldo de la Cuenta</label>
+                    <input type="number" className="form-control" id="balance" value={balance} onChange={(e) => setBalance(e.target.value)} />
                 </div>
 
                 <div className="mb-3">
                     <label htmlFor="choices" className="form-label">Elija el tipo de cuenta</label>
-                    <select className="form-select" aria-label="" id="choices">
-                        <option selected>Cuenta de Ahorro</option>
-                        <option value="1">Cuenta Corriente</option>
-                        <option value="2">Cuenta de Mercado Monetario</option>
-                        <option value="3">Cuenta de Jubilación Individual</option>
-                        <option value="4">Cuenta Comercial</option>
-                        <option value="5">Cuenta Nómina</option>
+                    <select className="form-select" aria-label="" id="choices" value={selectedChoice} onChange={handleChoiceChange}>
+                        <option value="Cuenta de Ahorro">Cuenta de Ahorro</option>
+                        <option value="Cuenta Corriente">Cuenta Corriente</option>
+                        <option value="Cuenta de Mercado Monetario">Cuenta de Mercado Monetario</option>
+                        <option value="Cuenta de Jubilación Individual">Cuenta de Jubilación Individual</option>
+                        <option value="Cuenta Comercial">Cuenta Comercial</option>
+                        <option value="Cuenta Nómina">Cuenta Nómina</option>
                     </select>
                 </div>
 
