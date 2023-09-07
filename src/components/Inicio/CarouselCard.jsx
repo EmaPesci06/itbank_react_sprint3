569e49610b96ae03afee2ef98485c447f241dad2
EmaPesci06 <emanuelpesci@gmail.com>
2023-09-07 00:38:08 -0300

    nada importante

--- src/components/Inicio/CarouselCard.jsx
+++ src/components/Inicio/CarouselCard.jsx
@@ -2,7 +2,7 @@ import Carousel from 'react-bootstrap/Carousel';
 
 function CarouselCard() {
     return (
-        <Carousel data-bs-theme="dark">
+        <Carousel data-bs-theme="dark" style={{ 'height': '500px' }}>
             <Carousel.Item>
                 <img
                     className="d-block w-100"
@@ -10,8 +10,6 @@ function CarouselCard() {
                     alt="First slide"
                 />
                 <Carousel.Caption>
-                    <h5>First slide label</h5>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
@@ -21,8 +19,6 @@ function CarouselCard() {
                     alt="Second slide"
                 />
                 <Carousel.Caption>
-                    <h5>Second slide label</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
@@ -32,10 +28,6 @@ function CarouselCard() {
                     alt="Third slide"
                 />
                 <Carousel.Caption>
-                    <h5>Third slide label</h5>
-                    <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
