36483b5b685904a271f3087592aa9a2be279b624
marneo23 <martin.al.rodriguez24@gmail.com>
2023-09-07 00:32:25 -0300

    Agregué scripts de Préstamos

--- src/components/Prestamo/Prestamo.jsx
+++ src/components/Prestamo/Prestamo.jsx
@@ -1,30 +1,81 @@
+import React, { useState } from 'react';
 
 export const Prestamo = () => {
-    return (
-        <main class="main">
-            <div class="containerPrestamo">
-                <h2 class="tituloPrestamo">Calculadora de Prestamos</h2>
-                <div class="calculator">
-                    <div class="containerLabel">
-                        <label for="montoPrestamo">Monto del Préstamo:</label>
-                        <input class="inputPrestamo" type="number" id="montoPrestamo" step="0.01" required />
-                    </div>
+  const [montoPrestamo, setMontoPrestamo] = useState('');
+  const [tasa, setTasa] = useState('');
+  const [años, setAños] = useState('');
+  const [resultado, setResultado] = useState('');
 
-                    <div class="containerLabel">
-                        <label for="tasa">Tasa de Interés (% anual):</label>
-                        <input class="inputPrestamo" type="number" id="tasa" step="0.01" required />
-                    </div>
+  const handleCalculate = () => {
+    const cantidadPrestamo = parseFloat(montoPrestamo);
+    const tasaDeInteres = parseFloat(tasa) / 100;
+    const loanTerm = parseFloat(años);
 
-                    <div class="containerLabel">
-                        <label for="años">Plazo del Préstamo (años):</label>
-                        <input class="inputPrestamo" type="number" id="años" required />
-                    </div>
-                    <button type="button" class="btn btn-primary" id="calculateButton">Calcular</button>
+    const interesMensual = tasaDeInteres / 12;
+    const meses = loanTerm * 12;
 
-                    <div id="result"></div>
-                </div>
-            </div>
-        </main>
+    const pagoMensual = (cantidadPrestamo * interesMensual) / (1 - Math.pow(1 + interesMensual, -meses));
 
-    )
-}
+    if (isNaN(pagoMensual)) {
+      setResultado(`Complete todos los campos`);
+    } else {
+      setResultado(`Pago Mensual: $${pagoMensual.toFixed(2)}`);
+    }
+  };
+
+  return (
+    <main className="main">
+      <div className="containerPrestamo">
+        <h2 className="tituloPrestamo">Calculadora de Préstamos</h2>
+        <div className="calculator">
+          <div className="containerLabel">
+            <label htmlFor="montoPrestamo">Monto del Préstamo:</label>
+            <input
+              className="inputPrestamo"
+              type="number"
+              id="montoPrestamo"
+              step="0.01"
+              required
+              value={montoPrestamo}
+              onChange={(e) => setMontoPrestamo(e.target.value)}
+            />
+          </div>
+
+          <div className="containerLabel">
+            <label htmlFor="tasa">Tasa de Interés (% anual):</label>
+            <input
+              className="inputPrestamo"
+              type="number"
+              id="tasa"
+              step="0.01"
+              required
+              value={tasa}
+              onChange={(e) => setTasa(e.target.value)}
+            />
+          </div>
+
+          <div className="containerLabel">
+            <label htmlFor="años">Plazo del Préstamo (años):</label>
+            <input
+              className="inputPrestamo"
+              type="number"
+              id="años"
+              required
+              value={años}
+              onChange={(e) => setAños(e.target.value)}
+            />
+          </div>
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={handleCalculate}
+          >
+            Calcular
+          </button>
+
+          <div id="result">{resultado}</div>
+        </div>
+      </div>
+    </main>
+  );
+};
